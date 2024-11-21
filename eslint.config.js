1d6c14011b2aa954bbf814a9f327bbc0fc13b85d
Aayushsingh2001 <as6994770@gmail.com>
2024-11-21 20:12:06 +0530

    Added jenkinsfile for building the react code

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/__tests__'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -17,7 +17,6 @@ export default [
         sourceType: 'module',
       },
     },
-    "extends": ["plugin:jest/recommended"],
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js', // Add this line if you have a setup file
+    exclude: [...configDefaults.exclude, 'e2e/*'], // Example of excluding certain files from tests
   },
 })
