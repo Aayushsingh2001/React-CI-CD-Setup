0e4405073f04b49c13fb1dce9a8ccb2e2e9afadb
Aayushsingh2001 <as6994770@gmail.com>
2024-11-21 18:08:48 +0530

    Added jenkinsfile for building the react code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { configs } from 'eslint-plugin-react'
-import { configDefaults } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
